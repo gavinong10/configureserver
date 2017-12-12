90fcc8b785f61a208f61743b1442460a93e58fe3
Gavin Ong <gavinong10@gmail.com>
2017-12-12 10:54:17 +0000

    git config

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 sudo yum -y install git
+git config --global user.email "gavinong10@gmail.com"
+git config --global user.name "Gavin Ong"
+ssh-keygen -t rsa -b 4096 -C "gavinong10@gmail.com"
+cat /home/ec2-user/.ssh/id_rsa.pub 
